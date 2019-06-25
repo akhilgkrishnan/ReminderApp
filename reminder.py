fde49e58618b2cf076e634e6a9d7306d5b7dbafa
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2019-06-25 23:34:16 +0530

    create database connection and add create and view functions

--- reminder.py
+++ reminder.py
@@ -1,45 +1,78 @@
 import mysql.connector
+from mysql.connector import Error
+import time 
+import os
 
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="reminder",
-  passwd="akhilgk",
-  database="sample"
-)
-mycursor = mydb.cursor()
-#mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
 
-def reminderCreate():
-    print("Enter the Date (DD/MM/YYY) :")
-    
-def reminderUpdate():
-def reminderView():        
-
-print("---Reminder Management System---")
-while True:
-    print("=================================")
-    print("Select the Choice")
-    print(
-        "1:Create Reminder\n"
-        "2:Update Reminder\n"
-        "3:View Reminder\n"
-        "4:Exit"        
+try:
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="reminder",
+        passwd="akhilgk",
+        database="sample"
     )
+    mycursor = mydb.cursor()
+    #mycursor.execute("CREATE TABLE reminder (createdtimestamp TIMESTAMP, taskname VARCHAR(100),taskmsg VARCHAR(255),taskdate DATE,tasktime TIME)")
+
+    def reminderCreate():
+        print("======Add New Reminder====")
+        taskname = input("Enter the Task Name :")
+        taskmsg = input("Enter the Task Message :")
+        taskdate =input("Enter the Task Date (YY-MM-DD) :")
+        tasktime =input("Enter the Time Time (HH:MM:SS) :")
+        sql= "INSERT INTO reminder (taskname,taskmsg,taskdate,tasktime) values (%s,%s,%s,%s)"
+        val = (taskname,taskmsg,taskdate,tasktime)
+        mycursor.execute(sql,val)
+        mydb.commit()
+        os.system('notify-send "You have a reminder"'+taskname) 
+
+
+  
     
-    choice = int(input("Enter the Choice :"))
-    print("=================================")
+    #def reminderUpdate():
+    def reminderView():
+        mycursor.execute("SELECT * FROM reminder")
+        myresult = mycursor.fetchall()
+        for row in myresult:
+            print("Reminder Timestamp = ", row[0], )
+            print("Remainder Name = ", row[1])
+            print("Remainder Message  = ", row[2])
+            print("Remainder Date  = ", row[3])
+            print("Remainder Time  = ", row[4], "\n")
+        mycursor.close()      
 
-    if(choice==1):
-        reminderCreate()
-        print("Create")
-    elif(choice==2):
-        def reminderUpdate():
-        print("Update")
-    elif(choice==3):
-        def reminderView(): 
-        print("View")
-    elif(choice==4):
-        break        
-    else:
-        print("Invalid Choice")
+    print("---Reminder Management System---")
+    while True:
+        print("=================================")
+        print("Select the Choice")
+        print(
+            "1:Create Reminder\n"
+            "2:Update Reminder\n"
+            "3:View Reminder\n"
+            "4:Exit"        
+        )
+    
+        choice = int(input("Enter the Choice :"))
+        print("=================================")
 
+        if(choice==1):
+            reminderCreate()
+            print("New Reminder added Successfully")
+        elif(choice==2):
+            #reminderUpdate():
+            print("Update")
+        elif(choice==3):
+            reminderView()
+            print("View")
+        elif(choice==4):
+            break        
+        else:
+            print("Invalid Choice")
+        remindercheck()
+except Error as e :
+    print ("Error while connecting to MySQL", e)
+finally:
+    #closing database connection.
+    if(mydb.is_connected()):
+        mydb.close()
+        print("MySQL connection is closed")
